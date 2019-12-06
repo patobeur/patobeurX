4c578cf9ae1e39a5117c71b82fdc8ac3114f62d3
Patobeur41 <48214878+patobeur@users.noreply.github.com>
2019-12-06 23:47:33 +0100

    touittters maj

--- www/patobeur_light/in/_in_touitter.php
+++ www/patobeur_light/in/_in_touitter.php
@@ -1,33 +1,14 @@
-            <!-- COVER -->
+            <!-- toO-it -->
             <div class="touitter">
                 <div class="touitters shadows outlined">
                     <div class="navigation">
-                        <button id="newtouit" class="menu">coucou</button>
-                        <div class="menu">Eveniet.</div>
-                        <div class="menu">Maiores.</div>
-                        <div class="menu">Excepturi?</div>
-                        <div class="menu">Modi.</div>
+                        <button id="newtouit" class="menu">NEW TOUIT</button>
+                        <div class="menu">Mafiche</div>
+                        <div class="menu">J'aime</div>
                     </div>
+                    <div id="console"></div>
 
-                    <div class="touittzone">
-                        <div class="fichetouit">
-                            <h1>Touit de </h1>
-                            <p>texte : </p>
-                        </div>
-                    </div>
-
-                    <div class="joker">
-                        <div class="joker2">
-                            <h1>Touitsdter</h1>
-                            <p>Join our community !</p>
-                            <button id="touiit" class="btn-touit">login</button>
-                            <div id="return"></div>
-                            <div class="form-footer">
-                                <p>Don't have account?</p>
-                            </div>
-                        <div id="console" class="consol"></div>
-                        </div>
-                    </div>
+                    <div id="touittzone" class="touittzone"></div>
                 </div>
             </div>
-            <!-- END-COVER -->
+            <!-- End's toO-it -->
--- www/patobeur_light/js/touitter.js
+++ www/patobeur_light/js/touitter.js
@@ -1,23 +1,68 @@
-document.addEventListener("DOMContentLoaded", addTouit())
-function addTouit() {
+document.addEventListener("DOMContentLoaded", addNewTouit())
+    var nbtouit = 0;
+    var qui = "#Patobeur";
+// -----------------------------------------------------------------------------------
+function addNewTouit() {
+    // waiting for refresh json content
     document.getElementById('console').innerHTML = "ok";
-}
-function addcoucou(name, message) {
-    document.getElementById('console').innerHTML = "coucou";
-    
-}
-function creatTouit(w,x){
-    let = blochtml = '<div class="fichetouit"><h1>'+w+'</h1><p>'+x+'</p></div>';
+
+    var NewTouiit = document.querySelector("#newtouit");
+    NewTouiit.addEventListener("click", clikage);
 }
 
+function clikage(){
+    nbtouit++;
+    // var query = {one: null,two: null};
+    var divtest = document.createElement("div");
+    divtest.className = "fiche";
+    rgbautoT(divtest);
+    divtest.innerHTML = creatTouit(qui,fauxtext(hitdice(5,25)),nbtouit,'fichetouit','');
 
-var elt = document.getElementById("newtouit");
-elt.onclick = function () {
-    // var query = {
-    //     login: null,
-    //     pwd: null
-    // };
-    // nodee = document.getElementById('touittzone')
-    let tm = creatTouit('Moi','coucou');
-    document.getElementById('touittzone').appendChild(tm); 
-};
+    var objTo = document.querySelector('#touittzone');
+    objTo.insertBefore(divtest,objTo.childNodes[0]);
+}
+
+// -----------------------------------------------------------------------------------
+function creatTouit(qui,message,nbtouit,classe,ide){
+    //date et heure du touiite ??
+    if (ide!=''){ide =' id="'+ide+'"';}
+    if (classe!=''){classe=' class="'+classe+'"';}
+    document.getElementById('console').innerHTML = nbtouit + " déjà touités";
+    return '<div'+ide+classe+'><h1>#'+nbtouit+"/"+qui+'</h1><p>'+message+'</p><p></p></div>';
+
+}
+// -----------------------------------------------------------------------------------
+function fauxtext(nbword){
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var phrase = '';
+    for (i = 0; i < nbword; i++){
+        nblettre = hitdice(2,9);
+        for (j=0; j < nblettre; j++){
+            phrase += alphabet[hitdice(0,15)];
+        }
+        phrase += " ";
+    }
+    phrase += ".";
+    return phrase;
+}
+function hitdice(min,max) {
+    return Math.floor(Math.random(min) * Math.floor(max));
+}
+// -----------------------------------------------------------------------------------
+function rgbauto() {
+    return 'rgb('+hitdice(0,255)+','+hitdice(0,255)+','+hitdice(0,255)+')';
+}
+function rgbautoT(objet) {
+    let R = hitdice(0,255);
+    let V = hitdice(0,255);
+    let B = hitdice(0,255);
+    let Nuance = R+V+B;
+    let couleur = 'rgba('+R+','+V+','+B+')';
+    objet.style.backgroundColor = couleur;
+    if (R < 180 && V < 180 && B < 180) {
+        objet.style.color = "white";
+    }
+    let elementa = document.createElement('p');
+    elementa.innerHTML = couleur;
+    objet.appendChild(elementa);
+}
