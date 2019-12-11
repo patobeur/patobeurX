7adfbc7f99c6187c1da1be0451d3d8ceac3c071a
Patobeur41 <48214878+patobeur@users.noreply.github.com>
2019-12-11 13:11:25 +0100

    add

---
+++ patobeur/_Db_Class_Default.php
@@ -0,0 +1,52 @@
+<?php
+    class Database{
+        private $MyDomaineAMoi = "";             
+        if ($_SERVER['HTTP_HOST'] == $MyDomaineAMoi){
+          private $host = "";
+          private $db_name = "";
+          private $username = "";
+          private $password = "";
+        }
+        elseif $_SERVER['HTTP_HOST'] == 'localhost'){
+          private $host = "localhost";
+          private $db_name = "";
+          private $username = "";
+          private $password = "";
+        }
+        else{
+          // DIE
+          die;
+        }
+        
+        private static $cont = null;
+        Public static $cont2 = null;
+        Public static $cont3 = array();
+    
+        public $conn;
+    
+        public function dbConnection(){
+            $this->conn = null;
+            try{
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch(PDOException $exception){
+                echo "Connection error: " . $exception->getMessage();
+            }
+            return $this->conn;
+        }
+    
+        public static function connect(){
+            // Autoriser une seule connexion pour toute la durée de l’accès
+            if ( null == self::$cont ){
+              try{
+                self::$cont = new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);
+              }
+              catch(PDOException $e){
+                die($e->getMessage());
+              }
+            }
+            return self::$cont;
+        }
+    }
+?>
